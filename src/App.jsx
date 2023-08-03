7d5ce707d8921408d6616973a62618ba66f747a3
ikbal <ikbalmondal062@gmail.com>
2023-08-03 21:35:26 +0530

    just commit

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is {count} Ikbal  
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
